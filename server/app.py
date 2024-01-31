a9d6754097c2b4268827117fb97d24dec602f5bf
Brandi L Mason <masonbrandi66@gmail.com>
2024-01-30 18:12:02 -0700

    added pycache to gitignore

--- server/app.py
+++ server/app.py
@@ -6,12 +6,9 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
-
 def display_data():
     resorts = get_ski_resort(app.debug)
     for resort in resorts['data']:
         resort["weather"] = get_weather(resort['location']['latitude'], resort['location']['longitude'])
         
-    return {
-        "resorts" : resorts
-        }
+    return { "resorts" : resorts }
